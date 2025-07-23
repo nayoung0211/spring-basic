760835034204cf563c3a04ff126cb5d27db75368
202504_parkny <tomato.202504.parkny@gmail.com>
2025-07-23 09:31:46 +0900

    basic-1

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,23 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixedDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId,String itemName,int itemPrice) {
+        Member member = memberRepository.findById(memberId); // 회원 정보를 받아온 뒤 조회
+        int discountPrice = discountPolicy.discount(member,itemPrice); // 받은 회원 정보를 할인 정책에 넘김
+
+        return new Order(memberId,itemName,itemPrice,discountPrice);
+    }
+
+}

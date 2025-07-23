5a3ca334fc2529efb52918dfa66d5555745227e7
nayoung <noela0202@gmail.com>
2025-07-23 10:27:40 +0900

    basic-25.07.23

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,8 +9,14 @@ import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    private final MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
+
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId,String itemName,int itemPrice) {
@@ -19,5 +25,4 @@ public class OrderServiceImpl implements OrderService {
 
         return new Order(memberId,itemName,itemPrice,discountPrice);
     }
-
 }
